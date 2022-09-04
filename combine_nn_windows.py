55671fe2e7c105e7ff24cc2f8ba754c8b281ea0b
AlbertoRosado1 <ar652820@ohio.edu>
2022-09-04 13:17:37 -0700

    now only select windows with loss vals lower than specified loss tolerance

--- combine_nn_windows.py
+++ combine_nn_windows.py
@@ -3,15 +3,30 @@ import numpy as np
 import healpy as hp
 from glob import glob
 import fitsio as ft
+import torch
 
+loss_tol = -58.5
+
+windows_l = []
+windows_all = dict()
 windows = dict()
 for region in ['bmzls', 'ndecals', 'sdecals']:
-    windows[region] = glob(f'/global/cscratch1/sd/arosado/test_sysnet/{region}_256/windows/window_model_*fits')
+    windows_all[region] = glob(f'/global/cscratch1/sd/arosado/test_sysnet/{region}_256/windows/window_model_*fits')
+    for i, w_fn in enumerate(windows_all[region]):
+        model_i = w_fn.split('_')[4]
+        randseed = w_fn.split('_')[5] 
+        snapshot_i = w_fn.split('_')[7].split('.')[0]
+        snapshot = torch.load(f'/global/cscratch1/sd/arosado/test_sysnet/{region}_256/model_{model_i}_{randseed}/snapshot_{snapshot_i}.pth.tar')
+        if snapshot['best_val_loss'] < loss_tol:
+            windows_l.append(w_fn) 
+    windows[region] = windows_l
+    windows_l = []
     print(region)
 
 
-nwindows = 1000 # ls -1 | wc -l for counting files in directory
+nwindows = min([len(windows[r]) for r in ['bmzls', 'ndecals', 'sdecals']])
 nside = 256
+
 for i in range(nwindows):
 
     count_i = np.zeros(12*nside*nside)
